1a51ed8e56113340794f4673b543735a5a8623fd
unknown <theprincemohit@gmail.com>
2020-11-21 11:35:11 +0530

    first commit

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,133 +1,118 @@
-import '../Home.css';
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import { createBrowserHistory } from 'history';
-import {  location } from "react-router-dom";
-import config from '../config/config';
 import getData from '../collection/getData';
-import Block from '../component/Block';
-import Filter from '../component/Filter';
-
-class Home extends  Component {
+import Block from './Block';
+import Filter from './Filter';
+import  '../Home.css';
+class Home extends Component {
   constructor(props) {
     super(props);
-    this.state= {
+    this.state = {
       listdata: [],
       loading: true,
-      filterBy : {year : [2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]},
-      params : {}
-    }
-     
-    
-    
-    
+      filterBy: { year: [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020] },
+      params: {}
+    };
+
+
+
+
   }
   componentDidMount() {
-   
-    
-    const query = window.location.search.substring(1)
-    
-       this.getDataFromAPI(query);
-       const urlParams = new URLSearchParams(query);
-       const filterSelected = {};
-       if(urlParams.get('launch_year'))
-       {
-        filterSelected.launch_year = urlParams.get('launch_year')
-       }
-       if(urlParams.get('launch_success'))
-       {
-        filterSelected.launch_success = Boolean(urlParams.get('launch_success'))
-       }
-       if(urlParams.get('land_success'))
-       {
-        filterSelected.land_success = Boolean(urlParams.get('land_success'))
-       }
-      // (urlParams.get('launch_year') != null) ? filterSelected.launch_year = urlParams.get('launch_year') : '';
+
+
+    const query = window.location.search.substring(1);
+
+    this.getDataFromAPI(query);
+    const urlParams = new URLSearchParams(query);
+    const filterSelected = {};
+    if (urlParams.get('launch_year')) {
+      filterSelected.launch_year = urlParams.get('launch_year');
+    }
+    if (urlParams.get('launch_success')) {
+      filterSelected.launch_success = Boolean(urlParams.get('launch_success'));
+    }
+    if (urlParams.get('land_success')) {
+      filterSelected.land_success = Boolean(urlParams.get('land_success'));
+    }
+    // (urlParams.get('launch_year') != null) ? filterSelected.launch_year = urlParams.get('launch_year') : '';
     //   (urlParams.get('launch_success') != null) ? filterSelected.launch_success = urlParams.get('launch_success') : '';
     //   (urlParams.get('land_success') != null) ? filterSelected.land_success = urlParams.get('land_success') : '';
-       console.log('urlParams',filterSelected ) 
-//const myParam = urlParams.get('myParam');
-this.setState({params : filterSelected});
+    console.log('urlParams', filterSelected);
+    //const myParam = urlParams.get('myParam');
+    this.setState({ params: filterSelected });
   }
-   
 
-  getDataFromAPI = async(query) => {
+
+  getDataFromAPI = async (query) => {
     const responce = await getData(query);
-    this.setState({listdata : responce, loading : false});
-        // console.log(responce) 
-  }  
+    this.setState({ listdata: responce, loading: false });
+    // console.log(responce) 
+  };
 
-  filterparams =  async (type,data) => {
+  filterparams = async (type, data) => {
 
-   
-    
-    var params = this.state.params
-   
-     if(params[type] == data) {
-       
-      delete params[type]
-      
-     }
-     else{
-      params[type]= data; 
-      
-     }  
-     
-    this.setState({params : params});
-      
-     var ueryString = '?';
-      for (var key in params)
-      {
-        ueryString += key+'='+params[key]+'&'
-      }
-      const history = createBrowserHistory();
-      history.push(ueryString)
-      const query = window.location.search.substring(1)
-    
-       this.getDataFromAPI(query);
-       
-      
-  }  
 
-  render() { 
-    return ( 
-        
-      <div class="content"> 
+
+    var params = this.state.params;
+
+    if (params[type] == data) {
+
+      delete params[type];
+
+    }
+    else {
+      params[type] = data;
+
+    }
+
+    this.setState({ params: params, loading: true });
+
+    var ueryString = '?';
+    for (var key in params) {
+      ueryString += key + '=' + params[key] + '&';
+    }
+    const history = createBrowserHistory();
+    history.push(ueryString);
+    const query = window.location.search.substring(1);
+
+    this.getDataFromAPI(query);
+
+
+  };
+
+  render() {
+    return (
+
+      <div class="content">
         <h3 className="title">SpaceX Launch Programme</h3>
         <div class="sidebar">
-         
-        <Filter 
-          FilterBy={this.state.filterBy} 
-          filterClick={this.filterparams}
-          activebtn={this.state.params}
-          />  
+
+          <Filter
+            FilterBy={this.state.filterBy}
+            filterClick={this.filterparams}
+            activebtn={this.state.params} />
         </div>
         <div class="main-content">
-          {
-            
-            (this.state.loading) ?  'Loading...'
+          {(this.state.loading) ? 'Loading...'
 
-            : 
+            :
 
-          (this.state.listdata.length > 0) ? this.state.listdata.map((data) => {
+            (this.state.listdata.length > 0) ? this.state.listdata.map((data) => {
               return (
                 <Block
-                data={data}
-                
-                /> 
-              )
-               
-}) : 'No Data Found' 
-         
+                  data={data} />
+              );
 
-          }
-        
-               
-    </div>
+            }) : 'No Data Found'}
+
+
+        </div>
         <div className="footer">Developed by : Mohit Kumar</div>
-    </div>
-    )
+      </div>
+    );
 
+  }
 }
 
-}
 export default Home;
